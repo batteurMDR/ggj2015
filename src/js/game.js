ab0fa230e613afddcf81e0d16c715a8d899b3517
smougel <sebastien.mougel@rentabiliweb.com>
2015-01-24 00:37:15 +0100

    background

--- src/js/game.js
+++ src/js/game.js
@@ -7,6 +7,12 @@ var game =
 	 init : function()
 	 {
 	 	toolbar.init();
+	 	this.setBackground();
+	 },
+
+	 setBackground : function()
+	 {
+	 	$('#screen').css("background-image", "url(img/backgrounds/background0.png)");
 	 }
 
 }
