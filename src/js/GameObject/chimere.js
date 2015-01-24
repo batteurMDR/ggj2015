06553c60ba3bcb2ef8d92d63e3aeed086c7a1b2c
System Administrator <root@iMac-de-Louis.local>
2015-01-24 19:44:28 +0100

    add css and visual

--- src/js/GameObject/chimere.js
+++ src/js/GameObject/chimere.js
@@ -13,8 +13,8 @@ function Chimere()
 			break;
 	}
     this.frame = game.frameHandler.earthFramesList[0];
-	this.height = 200;
-	this.width  = 400; 
+	this.height = 121;
+	this.width  = 145; 
     this.zIndex = this.frame.id;
     this.name = "chimere";    
     this.id = "Chimere"+Chimere.count;
@@ -22,9 +22,25 @@ function Chimere()
     this.maxY = this.frame.maxY;
     Chimere.count++;
 
-    this.spriteAnimator = function()
+    this.animate = function()
     {
 
+
+    }
+
+    this.spriteAnimator=function()
+    {
+    	$('#'+this.id).animateSprite({
+	        fps: 18,
+	        loop: true,
+	        animations: {
+            	walk: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        	},
+        	complete: function(){
+            	//alert('Sprite animation complete!');
+        	}
+        });
+        $('#'+this.id).animateSprite('play', 'walk');
     }
 }
 
