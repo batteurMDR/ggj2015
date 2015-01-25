64ebde60f4b616d3c209d3421e405a6b7cc4239a
System Administrator <root@iMac-de-Louis.local>
2015-01-25 01:34:26 +0100

    fix
    j'ai rectifier le problème du callback dans le helper nicebox

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,20 +1,19 @@
 function NicePopUP(sTitle,sText)
 {
-	alert('ok');
-	
-	console.log(arguments);
-	$('<div/>',
-                {
-                    "class":"nicebox",
-                    "data-depth":1.0
-                }
-        ).appendTo($('#screen')).html('<h1>'+sTitle+'</h1><p>'+sText+'</p>');
-
-	$('.nicebox').click(function()
-	{
+	if( arguments.length==3){
+		alert('1');
+		this.callback = arguments[2];
+		console.log(arguments[2]);
+		console.log(this.callback);
+	}
+	var self = this;
+	$('<div/>',{"class":"nicebox","data-depth":1.0}).appendTo($('#screen')).html('<h1>'+sTitle+'</h1><p>'+sText+'</p>').click(function(e){
+		e.preventDefault();
 		$(this).remove();
+		self.exec();	
 	});
 
-	if( arguments.length==3)
-		arguments[2]();
+	this.exec=function(){
+		this.callback();
+	}
 }
