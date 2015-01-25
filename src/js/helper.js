0f07612959878a8f2e24839f9490b84945df4576
System Administrator <root@iMac-de-Louis.local>
2015-01-25 02:22:56 +0100

    fix

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,28 +1,16 @@
-function NicePopUP(sTitle,sText,callback)
-{		
+function NicePopUP(sTitle,sText)
+{
 	if( arguments.length==3){
-			this.callback = arguments[2];
-		}
-	$('<div/>',
-                {
-                    "class":"nicebox",
-                    "data-depth":1.0
-                }
-        ).appendTo($('#screen')).html('<h1>'+sTitle+'</h1><p>'+sText+'</p>');
-
-	$('.nicebox').click(function()
-	{
-
-		var self = this;
-		$('<div/>',{"class":"nicebox","data-depth":1.0}).appendTo($('#screen')).html('<h1>'+sTitle+'</h1><p>'+sText+'</p>').click(function(e){
-			e.preventDefault();
-			$(this).remove();
-			self.exec();	
+		this.callback = arguments[2];
+	}
+	var self = this;
+	$('<div/>',{"class":"nicebox","data-depth":1.0}).appendTo($('#screen')).html('<h1>'+sTitle+'</h1><p>'+sText+'</p>').click(function(e){
+		e.preventDefault();
+		$(this).remove();
+		self.exec();	
 	});
-
 
 	this.exec=function(){
 		this.callback();
 	}
-
 }
